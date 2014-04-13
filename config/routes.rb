e6a7310ca214c17273210b4e93e3bc88bc625808
Raj Gokal <rajivgokal@gmail.com>
2014-04-13 12:57:46 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 DemoAp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
-  resources :microposts
+  get "users/new"
+  root 'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
 
+  resources :microposts
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
